2a30aac1a3a8c8c45c70cf4cd00f2bc01943547f
Joana <asiakom24@vp.pl>
2023-08-01 15:52:09 +0200

    plots

--- E1_plot.py
+++ E1_plot.py
@@ -30,69 +30,79 @@ sel_m = np.mean(sel, axis=0)
 # Accuracy
 s = 3
 cols = plt.cm.Blues(np.linspace(0.3,0.9,5))
-
-for c_id, c in enumerate(chunk_size):
-    fig, ax = plt.subplots(4,3, figsize=(18,10), sharex=True, sharey=True)
-    plt.suptitle('MNIST | chunk size: %i' % c)
+for mode_id, mode in enumerate(modes):
+    fig, ax = plt.subplots(4,4, figsize=(19,9), sharex=True, sharey=True)
+    plt.suptitle('MNIST | mode: %s' % mode)
 
     for n_c_id, n_c in enumerate(n_cycles):
-        for mode_id, mode in enumerate(modes):
+        for c_id, c in enumerate(chunk_size):
+
+            if n_c_id==0:
+                ax[n_c_id, c_id].set_title('size:%i' % c)
+            if c_id==0:
+                ax[n_c_id, c_id].set_ylabel('cycles:%i \naccuracy' % n_c, fontsize=12)
             
-            ax[n_c_id, mode_id].set_title('cycles:%i mode:%s' % (n_c, mode))
-            ax[n_c_id, mode_id].grid(ls=':')
+            ax[n_c_id, c_id].grid(ls=':')
             for method_id in range(6):
                 temp = accs_m[c_id, n_c_id, mode_id, :, method_id]
 
                 if method_id==5:
                     temp = accs_m[c_id, n_c_id, mode_id, :, -1]
-                    ax[n_c_id, mode_id].plot(gaussian_filter1d(temp, s), c='r', alpha=.7)
+                    ax[n_c_id, c_id].plot(gaussian_filter1d(temp, s), c='r', alpha=.7)
                 else:
-                    ax[n_c_id, mode_id].plot(gaussian_filter1d(temp, s), c=cols[method_id], alpha=0.5)
+                    ax[n_c_id, c_id].plot(gaussian_filter1d(temp, s), c=cols[method_id], alpha=0.5)
     
     plt.tight_layout()            
-    plt.savefig('fig/m_acc_cs%i.png' % c)
+    plt.savefig('fig/m_acc_%s.png' % mode)
 
 # Time
 
-for c_id, c in enumerate(chunk_size):
-    fig, ax = plt.subplots(4,3, figsize=(18,10), sharex=True, sharey=True)
-    plt.suptitle('MNIST | chunk size: %i' % c)
+for mode_id, mode in enumerate(modes):
+    fig, ax = plt.subplots(4,4, figsize=(19,9), sharex=True, sharey=True)
+    plt.suptitle('MNIST | mode: %s' % mode)
 
     for n_c_id, n_c in enumerate(n_cycles):
-        for mode_id, mode in enumerate(modes):
+        for c_id, c in enumerate(chunk_size):
             
-            ax[n_c_id, mode_id].set_title('cycles:%i mode:%s' % (n_c, mode))
-            ax[n_c_id, mode_id].grid(ls=':')
+            if n_c_id==0:
+                ax[n_c_id, c_id].set_title('size:%i' % c)
+            if c_id==0:
+                ax[n_c_id, c_id].set_ylabel('cycles:%i \ntime' % n_c, fontsize=12)
+            
+            ax[n_c_id, c_id].grid(ls=':')
             for method_id in range(6):
                 temp = times_m[c_id, n_c_id, mode_id, :, method_id]
 
                 if method_id==5:
                     temp = times_m[c_id, n_c_id, mode_id, :, -1]
-                    ax[n_c_id, mode_id].plot(gaussian_filter1d(temp, s), c='r', alpha=.7)
+                    ax[n_c_id, c_id].plot(gaussian_filter1d(temp, s), c='r', alpha=.7)
                 else:
-                    ax[n_c_id, mode_id].plot(gaussian_filter1d(temp, s), c=cols[method_id], alpha=0.5)
+                    ax[n_c_id, c_id].plot(gaussian_filter1d(temp, s), c=cols[method_id], alpha=0.5)
     
     plt.tight_layout()                          
-    plt.savefig('fig/m_time_cs%i.png' % c)
+    plt.savefig('fig/m_time_%s.png' % mode)
     
     
 # Selection
 
-for c_id, c in enumerate(chunk_size):
-    fig, ax = plt.subplots(4,3, figsize=(18,10), sharex=True, sharey=True)
-    plt.suptitle('MNIST | chunk size: %i' % c)
+for mode_id, mode in enumerate(modes):
+    fig, ax = plt.subplots(4,4, figsize=(19,9), sharex=True, sharey=True)
+    plt.suptitle('MNIST | mode: %s' % mode)
     
     for n_c_id, n_c in enumerate(n_cycles):
-        for mode_id, mode in enumerate(modes):
+        for c_id, c in enumerate(chunk_size):
             
-            ax[n_c_id, mode_id].set_title('chunks:%i mode:%s' % (n_c, mode))
-            ax[n_c_id, mode_id].grid(ls=':')
+            if n_c_id==0:
+                ax[n_c_id, c_id].set_title('size:%i' % c)
+            if c_id==0:
+                ax[n_c_id, c_id].set_ylabel('cycles:%i \nclf index' % n_c, fontsize=12)
+            ax[n_c_id, c_id].grid(ls=':')
 
             temp = sel_m[c_id, n_c_id, mode_id]
-            ax[n_c_id, mode_id].scatter(np.arange(n_chunks), temp, c='cornflowerblue', alpha=0.5)
+            ax[n_c_id, c_id].scatter(np.arange(n_chunks), temp, c='cornflowerblue', alpha=0.5)
 
     plt.tight_layout()            
-    plt.savefig('fig/m_sel_cs%i.png' % c)
+    plt.savefig('fig/m_sel_%s.png' % mode)
 
 
 ### ### ### ### ### ### ### ###
@@ -109,66 +119,79 @@ accs_m = np.mean(accs, axis=0)
 times_m = np.mean(times, axis=0)
 sel_m = np.mean(sel, axis=0) 
 
+cols = plt.cm.Blues(np.linspace(0.3,0.9,6))
 
 # Accuracy
 
-for c_id, c in enumerate(chunk_size):
-    fig, ax = plt.subplots(4,3, figsize=(18,10), sharex=True, sharey=True)
-    plt.suptitle('SVHN | chunk size: %i' % c)
+for mode_id, mode in enumerate(modes):
+    fig, ax = plt.subplots(4,4, figsize=(19,9), sharex=True, sharey=True)
+    plt.suptitle('SVHN | mode: %s' % mode)
 
     for n_c_id, n_c in enumerate(n_cycles):
-        for mode_id, mode in enumerate(modes):
-            
-            ax[n_c_id, mode_id].set_title('cycles:%i mode:%s' % (n_c, mode))
-            ax[n_c_id, mode_id].grid(ls=':')
+        for c_id, c in enumerate(chunk_size):
+
+            if n_c_id==0:
+                ax[n_c_id, c_id].set_title('size:%i' % c)
+            if c_id==0:
+                ax[n_c_id, c_id].set_ylabel('cycles:%i \naccuracy' % n_c, fontsize=12)
+                
+            ax[n_c_id, c_id].grid(ls=':')
             for method_id in range(7):
                 temp = accs_m[c_id, n_c_id, mode_id, :, method_id]
 
                 if method_id==6:
-                    ax[n_c_id, mode_id].plot(gaussian_filter1d(temp, s), c='r', alpha=.7)
+                    ax[n_c_id, c_id].plot(gaussian_filter1d(temp, s), c='r', alpha=.7)
                 else:
-                    ax[n_c_id, mode_id].plot(gaussian_filter1d(temp, s), c=cols[method_id], alpha=0.5)
+                    ax[n_c_id, c_id].plot(gaussian_filter1d(temp, s), c=cols[method_id], alpha=0.5)
     
     plt.tight_layout()                             
-    plt.savefig('fig/acc_cs%i.png' % c)
+    plt.savefig('fig/acc_%s.png' % mode)
 
 # Time
 
-for c_id, c in enumerate(chunk_size):
-    fig, ax = plt.subplots(4,3, figsize=(18,10), sharex=True, sharey=True)
-    plt.suptitle('SVHN | chunk size: %i' % c)
+for mode_id, mode in enumerate(modes):
+    fig, ax = plt.subplots(4,4, figsize=(19,9), sharex=True, sharey=True)
+    plt.suptitle('SVHN | mode: %s' % mode)
 
     for n_c_id, n_c in enumerate(n_cycles):
-        for mode_id, mode in enumerate(modes):
+        for c_id, c in enumerate(chunk_size):
             
-            ax[n_c_id, mode_id].set_title('cycles:%i mode:%s' % (n_c, mode))
-            ax[n_c_id, mode_id].grid(ls=':')
+            if n_c_id==0:
+                ax[n_c_id, c_id].set_title('size:%i' % c)
+            if c_id==0:
+                ax[n_c_id, c_id].set_ylabel('cycles:%i \ntime' % n_c, fontsize=12)
+                
+            ax[n_c_id, c_id].grid(ls=':')
             for method_id in range(7):
                 temp = times_m[c_id, n_c_id, mode_id, :, method_id]
 
                 if method_id==6:
-                    ax[n_c_id, mode_id].plot(gaussian_filter1d(temp, s), c='r', alpha=.7)
+                    ax[n_c_id, c_id].plot(gaussian_filter1d(temp, s), c='r', alpha=.7)
                 else:
-                    ax[n_c_id, mode_id].plot(gaussian_filter1d(temp, s), c=cols[method_id], alpha=0.5)
+                    ax[n_c_id, c_id].plot(gaussian_filter1d(temp, s), c=cols[method_id], alpha=0.5)
     
     plt.tight_layout()                               
-    plt.savefig('fig/time_cs%i.png' % c)
+    plt.savefig('fig/time_%s.png' % mode)
     
     
 # Selection
 
-for c_id, c in enumerate(chunk_size):
-    fig, ax = plt.subplots(4,3, figsize=(18,10), sharex=True, sharey=True)
-    plt.suptitle('SVHN | chunk size: %i' % c)
+for mode_id, mode in enumerate(modes):
+    fig, ax = plt.subplots(4,4, figsize=(19,9), sharex=True, sharey=True)
+    plt.suptitle('SVHN | mode: %s' % mode)
     
     for n_c_id, n_c in enumerate(n_cycles):
-        for mode_id, mode in enumerate(modes):
+        for c_id, c in enumerate(chunk_size):
             
-            ax[n_c_id, mode_id].set_title('chunks:%i mode:%s' % (n_c, mode))
-            ax[n_c_id, mode_id].grid(ls=':')
+            if n_c_id==0:
+                ax[n_c_id, c_id].set_title('size:%i' % c)
+            if c_id==0:
+                ax[n_c_id, c_id].set_ylabel('cycles:%i \nclf index' % n_c, fontsize=12)
+                
+            ax[n_c_id, c_id].grid(ls=':')
 
             temp = sel_m[c_id, n_c_id, mode_id]
-            ax[n_c_id, mode_id].scatter(np.arange(n_chunks), temp, c='cornflowerblue', alpha=0.5)
+            ax[n_c_id, c_id].scatter(np.arange(n_chunks), temp, c='cornflowerblue', alpha=0.5)
     
     plt.tight_layout()            
-    plt.savefig('fig/sel_cs%i.png' % c)
+    plt.savefig('fig/sel_%s.png' % mode)
